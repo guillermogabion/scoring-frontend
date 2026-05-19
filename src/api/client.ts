f54484aeaf0eb1aa0dff2401726be99ae9409775
Gab <gabionian7@gmail.com>
2026-05-19 08:13:05 +0800

    change api url

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const client = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://scoring-backend-6p9h.vercel.app/api',
   headers: { 'Content-Type': 'application/json' },
 });
 
